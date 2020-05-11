2e91c4e8c0b344dcdf2f3d85276a5b06866dd45e
Leomiku <1207184332@qq.com>
2020-05-11 22:24:47 +0800

    new feature: show test points, test state: no

--- app/javascript/src/auto_tests/components/test_points.vue
+++ app/javascript/src/auto_tests/components/test_points.vue
@@ -1,8 +1,25 @@
 <template>
-    
+  <div class="test-points small-container">
+      <div class="page-title-holder">
+          <div class="page-title">
+              评测点列表
+          </div>
+      </div>
+      <el-card v-for="point in points" :key="point" class="point-card">
+          <div slot="header" class="point-card-header">
+              <h3>测试点{{point["index"]}}</h3>
+          </div>
+          <div class="point-card-body">
+              <h4>输入</h4>
+              <fold-text text="point['input']"></fold-text>
+              <h4>期望输出</h4>
+              <fold-text text="point['expected_output']"></fold-text>
+          </div>
+  </div>
 </template>
 
 <script>
+import FoldText from "../../text_component/fold_unfold_test.vue";
 
 export default {
     data() {
@@ -12,9 +29,24 @@ export default {
         }
     },
 
+    components() {
+        FoldText
+    },
+
     mounted() {
         const point_list = JSON.parse(this.$el.dataset.points);
-        
+        if (point_list.length > 0)
+        {
+            for (var index in point_list)
+            {
+                this.points.append({
+                    index: index,
+                    input:  point_list[index]["input"],
+                    expected_output: point_list[index]["expected_output"]
+                    })
+            }
+            this.has_points = true;
+        }
     }
 }
 
---
+++ app/javascript/src/text_component/fold_unfold_text.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="text-content">
+            <p v-if="!need_unfold">
+                {{text}}
+            </p>
+            <template v-if="need_unfold">
+                <template v-if="fold">
+                    <p>{{content.slice(0, 40) + "..."}}</p>
+                </template>
+                <template v-else="">
+                    <p>{{content}}</p>
+                </template>
+                <el-button type="text" @click="change_state">{{fold?'展开':'收起'}}</el-button>
+            </template>
+        </div>
+</template>
+
+<script>
+export default {
+    props: {
+        text: {
+            type: String,
+            required: true
+        }
+    },
+
+    data() {
+        return {
+            need_unfold: false,
+            fold: true
+        }
+    },
+
+    mounted() {
+        if (text.length > 40) {
+            need_unfold = true;
+        }
+    },
+
+    methods: {
+        change_state() {
+            fold = !fold;
+        }
+    }
+}
+</script>
