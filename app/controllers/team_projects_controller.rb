ee11e8c1eb0cf309417d64b755149c24ac906cc3
Kennnnnnji <kenwong1999@163.com>
2020-05-20 19:36:58 +0800

    batch team V0.2

--- app/controllers/team_projects_controller.rb
+++ app/controllers/team_projects_controller.rb
@@ -54,6 +54,57 @@ class TeamProjectsController < ApplicationController
     puts '**********'
   end
 
+  def create_team_project()
+    save_team_projects = TeamProject.new
+    @team_project = params[:team_project]
+
+    # look up for duplicate projects
+    @@dup_proj = false
+    TeamProject.all.each do |a_project|
+      if a_project[:name] == @team_project['name']
+        @@dup_proj = true
+        redirect_to new_classroom_team_project_path
+        return
+      end
+    end
+    @team_project[:initialize_with_readme] = !!@team_project[:initialize_with_readme]
+    classroom = Classroom.find(params[:classroom_id])
+    team_project = {
+        name: @team_project['name'],
+        path: @team_project['path'],
+        description: @team_project['description'],
+        initialize_with_readme: @team_project[:initialize_with_readme],
+        namespace_id: classroom.team_project_subgroup_id
+    }
+    # 以 owner 身份创建项目
+    owner = classroom.users.where(role: 'teacher').first
+    @team_project = create_project_as_owner team_project, owner.gitlab_id
+    # 当前用户添加为 Maintainer
+    # now we assume that only teacher could create project
+    # therefore, we should add all teachers in the classroom and the group member
+    # to this repo as Maintainer
+    member_gitlab_id = []
+    @team_project_ori = params[:team_project]
+    @team_project_ori[:members].each do |member|
+      member_gitlab_id.append(User.find(member).gitlab_id)
+    end
+
+    save_team_projects.gitlab_id = @team_project['id']
+    save_team_projects.classroom_id = params[:classroom_id]
+    save_team_projects.save
+
+    member_gitlab_id.each do |gitlab_id|
+      if gitlab_id != owner.gitlab_id
+        add_project_master_as_owner @team_project['id'], gitlab_id, owner.gitlab_id
+        # projects_service.add_user_as_project_maintainer(@team_project['id'], gitlab_id)
+      end
+    end
+  rescue RestClient::BadRequest => e
+    @errors = ['BadRequest, 可能名称或地址包含非法字符']
+    @@errors_save = @errors
+    redirect_to new_classroom_team_project_path
+  end
+
   def create_team_projects_batch
     id_str = params[:teamForm]['text']
     debug(id_str.split("\n"))
